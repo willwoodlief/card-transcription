a86d6972c2d65cf04736885b2492b2921755b3b9
willwoodlief <willwoodlief@gmail.com>
2016-11-12 14:35:50 -0600

    Json->new and converts json with encoded newlines
    also added beter error alerts through sns

--- pages/upload_from_bucket.php
+++ pages/upload_from_bucket.php
@@ -9,13 +9,27 @@
 require_once '../users/init.php';
 require_once $abs_us_root.$us_url_root.'/users/includes/header_json.php';
 require_once $abs_us_root.$us_url_root.'pages/helpers/pages_helper.php';
+$screamer = true;
+
+if ($screamer) {
+    $post = file_get_contents('php://input');
+    publish_to_sns("Post Dump of new->json", $post);
+}
+
 
 if (!Input::get('Message')) {
-    $getnice = $_POST;
-    if (!$getnice) {
-        $getnice = null;
+    if (isset($_POST["Message"])) {
+        $message = $_POST['Message'];
+    } elseif (isset($_GET["Message"])) {
+        $message = $_GET['Message'];
+    } else {
+        $message = null;
     }
-    $debug = array('message'=> 'Did not find the message param', 'get params' => $getnice);
+    $debug = array('message'=> 'Did not find the message param', 'get params' => $message);
+    if ($screamer) {
+
+        publish_to_sns("Post Dump of new->json", $debug);
+    }
     printErrorJSONAndDie($debug);
 }
 
@@ -32,12 +46,27 @@ try {
 
     $job = null;
     if ($message) {
+        $o_message = to_utf8(trim($message));
+        //unescape newlines from \" to "
+        $message = str_replace('\"','"',$o_message);
         $job = json_decode($message);
-    }
+        if (!$job) {
+            $what = get_json_last_err_string();
+            $debug = array('message'=> 'Could not convert message to json',
+                            'json error' => $what,
+                             'json_attempted'=>$message   );
+            if ($screamer) {
 
-    if (!$job) {
-        $err = ['json'=>$message, 'json_error'=>get_json_last_err_string()];
-        printErrorJSONAndDie($err);
+                publish_to_sns("Error Converting Json of new->json", $debug);
+            }
+            printErrorJSONAndDie($debug);
+        }
+    } else {
+        if ($screamer) {
+
+            publish_to_sns("Logic Error in new->json", '..?...');
+        }
+        printErrorJSONAndDie('Logic Error');
     }
 
 //we ignore subject
@@ -63,6 +92,10 @@ try {
     printOkJSONAndDie('images put into queue; nid is ' . $nid);
 }
 catch(Exception $e) {
+    if ($screamer) {
+        $debug = array('message'=> $e->getMessage(), 'trace' => $e->getTrace());
+        publish_to_sns("Error in new->json", $debug);
+    }
     printErrorJSONAndDie('could not insert job: '. $e->getMessage() . "\n" . $e->getTraceAsString());
 }
 
