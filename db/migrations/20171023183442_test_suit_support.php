2f770bfbd7e1b2f969605f2945c322332cad4f4f
willwoodlief <willwoodlief@gmail.com>
2017-10-23 19:38:59 -0500

    Created database table migrations for test suits. Updated phinx
    Updated Phix to 8.1
    phinx is now part of cakephp, but I use it as a stand alone in the transcription project.
    The new version adds more features
    and the change log is at https://github.com/cakephp/phinx/blob/master/CHANGELOG.md
    So, I replaced the older phar with the new one in this directory.
    
    The steps to do this is as follows (in case you want to do add it elsewhere, you can just copy the bin/phinx.phar in the project ).
    Also, these steps can be repeated with future version upgrades. Just change the branch arg in the clone command below
    
    * go outside the directories of this project and make a folder you can delete after all this
    * go inside that new folder
    * git clone https://github.com/cakephp/phinx.git --branch v0.9.1
    * cd phinx
    * php composer.phar install  (I have composer set up to run on the command line)
    * curl -LSs https://box-project.github.io/box2/installer.php | php (the software to make this a phar)
    * php box.phar build
    
    this will make a phinx-v0.9.1.phar, rename it to phinx.phar, give it execute privledge, and move  where needed, delete temp directory
    the version when run will say 0.8.1 (older one was 0.6.4)
    
    Created Database Tables for the tests (6 tables)
    
    Workflow is as follows
    
    1) when a test is run, it finds itself in the database, in the table tests. If cannot find itself exits
    2) It fills out and saves a new test_runs, and includes the starting time
    3) If there are any resources (user or transaction), it finds them in the seeds table
    4) It marks any resources as locked in the test_resource_locks. By making and saving a new row.If cannot get a lock then exits
    5) It pulls in the seed data for each resource and updates all the fields each resource
    6) The test is then run
    7) The result of the test (console message and pass fail status), is saved to the test_runs it created, as well as the end time
    8) Any resources it locked is unlocked by putting in a lock_time_end value
    
    Later on, we can have a cron job scanning the test_runs table, and any production test that fails can trigger a notice
    
    The tables are in phinx migration format, all the columns are commented.
    And there are some triggers to make sure the enumerations are kept accurate, to prevent typos
    All tables are in mb4 mysql and maria unicode format
    I added configs for the server to allow it to be mb4 also, if needed

---
+++ db/migrations/20171023183442_test_suit_support.php
@@ -0,0 +1,247 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class TestSuitSupport extends AbstractMigration
+{
+    /**
+     note for table encoding. Am using full unicode support, but for mysql server 5.5, unless set with different options in the config, will keep index sizes to less than 767
+     * this for strings that have keys, I keep the keys themselves to 150 (4 bytes a character *150) = 600
+     * see db/server_config/notes.txt
+     * and then do a migration to make all tables ALTER TABLE (ROW_FORMAT=DYNAMIC)
+     */
+    public function up()
+    {
+        ########################################Tests##############################################
+        $table = $this->table('tests', array('collation' => 'utf8mb4_unicode_ci','encoding'=>'utf8mb4','comment'=> "This defines each of the tests done"));
+        $table->addColumn('test_name', 'string', array('after' => 'id','limit' => 255,'null' => false,'comment'=>"the name of the test"))
+            ->addColumn('test_group_minor_name', 'string', array('after' => 'test_name','limit' => 255,'null' => false,'comment'=>"smaller groups of a few related tests"))
+            ->addColumn('test_group_major_name', 'string', array('after' => 'test_group_minor_name','limit' => 255,'null' => false,'comment'=>"larger groups holding smaller groups"))
+            ->addColumn('description', 'text', array('null'=>true,'default'=>null,'comment'=>"optional notes explaining the test"))
+            ->addIndex(array('test_name'), array('unique' => true,'limit'=>150))
+            ->addIndex(array('test_group_minor_name'), array('unique' => false,'limit'=>150))
+            ->addIndex(array('test_group_major_name'), array('unique' => false,'limit'=>150))
+            ->addIndex(array('description'), array('unique' => false,'limit'=>150))
+            ->create();
+
+        #######################################Test Runs###################################################
+
+        $table = $this->table('test_runs', array('collation' => 'utf8mb4_unicode_ci','encoding'=>'utf8mb4','comment'=> "This marks each time a test is actually run, the test output , and if it passed or not"));
+
+        $table->addColumn('has_passed', 'boolean', ['null' => false,'default'=>false,'length'=>1, 'signed' => false, 'comment'=>'if the test passed then not zero'])
+            ->addColumn('test_time_start', 'datetime', array('null' => false,'comment'=> "The start time of the run, add it when creating row"))
+            ->addColumn('test_time_end', 'datetime', array('null' => true,'default'=>null,'comment'=> "The end time of the run, update this rown when the test ends"))
+            ->addColumn('machine_id', 'string', array('limit' => 255,'null' => false,'comment'=>"the host name of the machine the tests are running on"))
+            ->addColumn('machine_type', 'string', array('limit' => 255,'null' => false,'comment'=>"what kind of environment, valid values are [development,production]"))
+            ->addColumn('test_type', 'string', array('limit' => 255,'null' => false,'comment'=>"what kind of test, valid values are [cronjob,manual,githook,automatic,other]"))
+            ->addColumn('unit_output', 'text', array('null'=>true,'default'=>null,'comment'=>"the console output of the test is saved here"))
+            ->addColumn('notes', 'text', array('null'=>true,'default'=>null,'comment'=>"this can be automaticly entered for certain tests or manually added to explain, or update a status and explanation of a test run"))
+            ->addIndex(array('test_time_start'), array('unique' => false))
+            ->addIndex(array('test_time_end'), array('unique' => false))
+            ->addIndex(array('has_passed'), array('unique' => false))
+            ->addIndex(array('machine_id'), array('unique' => false,'limit'=>150))
+            ->addIndex(array('machine_type'), array('unique' => false,'limit'=>150))
+            ->addIndex(array('test_type'), array('unique' => false,'limit'=>150))
+            ->addIndex(array('unit_output'), array('unique' => false,'limit'=>150))
+            ->addIndex(array('notes'), array('unique' => false,'limit'=>150))
+            ->create();
+
+        $trigger = <<<SQL
+        CREATE TRIGGER trigger_before_update_test_runs
+            BEFORE UPDATE ON test_runs
+            FOR EACH ROW
+        BEGIN
+          IF NEW.machine_type not in ('development','production')
+          THEN
+               SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Cannot add or update row in test_runs: machine_type has to be one of [development,production]';
+          END IF;
+          
+          IF NEW.test_type not in ('cronjob','manual','githook','automatic','other')
+          THEN
+               SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Cannot add or update row in test_runs: test_type has to be one of [cronjob,manual,githook,automatic,other]';
+          END IF;
+          
+        END
+SQL;
+        $this->execute($trigger);
+
+
+        $trigger = <<<SQL
+        CREATE TRIGGER trigger_before_create_test_runs
+            BEFORE INSERT ON test_runs
+            FOR EACH ROW
+        BEGIN
+          IF NEW.machine_type not in ('development','production')
+          THEN
+               SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Cannot add or update row in test_runs: machine_type has to be one of [development,production]';
+          END IF;
+          
+          IF NEW.test_type not in ('cronjob','manual','githook','automatic','other')
+          THEN
+               SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Cannot add or update row in test_runs: test_type has to be one of [cronjob,manual,githook,automatic,other]';
+          END IF;
+          
+        END
+SQL;
+        $this->execute($trigger);
+
+
+        ################################Test Resources#########################################
+
+        $table = $this->table('test_resources', array('collation' => 'utf8mb4_unicode_ci','encoding'=>'utf8mb4','comment'=> "This lists the resources used in the tests. For example users and transcriptions. For seeding and locking"));
+
+        $table->addColumn('type_resource', 'string', array('limit' => 255,'null' => false,'comment'=>"what kind of resource, valid values are [user,transcription], the type tells which table the id next is associated with"))
+            ->addColumn('resource_id', 'integer', array('null' => false,'comment'=>"this is primary id of the resource, look at type_resource to see which table, these can be in different databases so no fk set"))
+            ->addColumn('subtype', 'string', array('limit' => 255,'null'=>true,'default'=>null,'comment'=>"allow the resource to be identified for the role it plays: see testing docs for more info"))
+            ->addColumn('notes', 'text', array('null'=>true,'default'=>null,'comment'=>"any notes for this resource, including explaining why it is used"))
+            ->addIndex(array('type_resource'), array('unique' => false,'limit'=>150))
+            ->addIndex(array('resource_id'), array('unique' => false))
+            ->addIndex(array('subtype'), array('unique' => false,'limit'=>150))
+            ->addIndex(array('notes'), array('unique' => false,'limit'=>150))
+            ->create();
+
+        $trigger = <<<SQL
+        CREATE TRIGGER trigger_before_update_test_resources
+            BEFORE UPDATE ON test_resources
+            FOR EACH ROW
+        BEGIN
+          IF NEW.type_resource not in ('user','transcription')
+          THEN
+               SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Cannot add or update row in test_resources: type_resource has to be one of [user,transcription]';
+          END IF;
+          
+          
+        END
+SQL;
+        $this->execute($trigger);
+
+
+        $trigger = <<<SQL
+        CREATE TRIGGER trigger_before_create_test_resources
+            BEFORE INSERT ON test_resources
+            FOR EACH ROW
+        BEGIN
+          IF NEW.type_resource not in ('user','transcription')
+          THEN
+               SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Cannot add or update row in test_resources: type_resource has to be one of [user,transcription]';
+          END IF;
+          
+          
+          
+        END
+SQL;
+        $this->execute($trigger);
+
+
+        ####################################### Test Resource Locks#####################################################
+
+        $table = $this->table('test_resource_locks', array('collation' => 'utf8mb4_unicode_ci','encoding'=>'utf8mb4','comment'=> "This allows resource locking and sharing between the different tests"));
+        $table->addColumn('test_resource_id', 'integer', ['null' => false,'comment'=>"foreign key for test resource"])
+            ->addColumn('test_run_id', 'integer', ['null' => false,'comment'=>"foreign key for test run"])
+            ->addColumn('lock_time_start', 'datetime', array('null' => false,'comment'=> "The start time of the run, add it when creating row"))
+            ->addColumn('lock_time_end', 'datetime', array('null' => true,'default'=>null,'comment'=> "The end time of the run, update this rown when the test ends"))
+            ->addForeignKey('test_run_id', 'test_runs', 'id', array('delete'=> 'RESTRICT', 'update'=> 'CASCADE'))
+            ->addForeignKey('test_resource_id', 'test_resources', 'id', array('delete'=> 'RESTRICT', 'update'=> 'CASCADE'))
+            ->addIndex(array('lock_time_end'), array('unique' => false))
+            ->addIndex(array('lock_time_start'), array('unique' => false))
+            ->create();
+
+
+        ####################################### Test Seeds ##################################
+        $table = $this->table('test_seeds', array('collation' => 'utf8mb4_unicode_ci','encoding'=>'utf8mb4','comment'=> "The data that a resource is set to for a certain test. Also tells the test which resources to use"));
+
+        $table->addColumn('test_run_id', 'integer', ['null' => true,'comment'=>"foreign key for test run. Can be null if used as an ancestor"])
+            ->addColumn('test_resource_id', 'integer', ['null' => true,'comment'=>"foreign key for test resource. Can be null if used as an ancestor"])
+            ->addColumn('subtype', 'string', array('limit' => 255,'null'=>true,'default'=>null,'comment'=>"allows the test to figure out how to use the resources. See each test for information"))
+            ->addColumn('ancestors', 'string', array('limit' => 255,'null'=>true,'default'=>null,'comment'=>"any ancestor data is filled in first, this allows tweaking of test sets for other tests without reintering all the data"))
+            ->addColumn('notes', 'text', array('null'=>true,'default'=>null,'comment'=>"any notes for this seeding, like explaining that its used as a generic seed, or what the strategy of the data will be"))
+            ->addForeignKey('test_run_id', 'test_runs', 'id', array('delete'=> 'RESTRICT', 'update'=> 'CASCADE'))
+            ->addForeignKey('test_resource_id', 'test_resources', 'id', array('delete'=> 'RESTRICT', 'update'=> 'CASCADE'))
+            ->addIndex(array('ancestors'), array('unique' => false,'limit'=>160))
+            ->addIndex(array('subtype'), array('unique' => false,'limit'=>160))
+            ->addIndex(array('test_run_id','test_resource_id'), array('unique' => true,'name'=>'uidx_no_dupicate_resources_per_test'))
+            ->addIndex(array('notes'), array('unique' => false,'limit'=>150))
+            ->create();
+
+
+        ######################## Test Seed Values ###################################333
+        $table = $this->table('test_seed_values', array('collation' => 'utf8mb4_unicode_ci','encoding'=>'utf8mb4','comment'=> "The actual seed data"));
+
+        $table->addColumn('test_seed_id', 'integer', ['null' => true,'comment'=>"foreign key for the seed"])
+            ->addColumn('data_type', 'string', array('limit' => 255,'null'=>false,'comment'=>"allows casting of data, if necessary. Needs to be one of [int,boolean,float,date,time,datetime,timestamp,unix_timestamp,string"))
+            ->addColumn('column_name', 'string', array('limit' => 150,'null'=>false,'comment'=>"the column name to be updated"))
+            ->addColumn('data', 'text', array('null'=>true,'default'=>null,'comment'=>"the value of the column. Null here will be made null there"))
+            ->addForeignKey('test_seed_id', 'test_seeds', 'id', array('delete'=> 'RESTRICT', 'update'=> 'CASCADE'))
+            ->addIndex(array('data_type'), array('unique' => false,'limit'=>150))
+            ->addIndex(array('column_name'), array('unique' => false,'limit'=>150))
+            ->addIndex(array('column_name','test_seed_id'), array('unique' => true,'name'=>'uidx_no_duplicate_columns_in_seeds'))
+            ->create();
+
+
+        $trigger = <<<SQL
+        CREATE TRIGGER trigger_before_update_test_seed_values
+            BEFORE UPDATE ON test_seed_values
+            FOR EACH ROW
+        BEGIN
+          IF NEW.data_type not in ('int','boolean','float','date','time','datetime','timestamp','unix_timestamp','string')
+          THEN
+               SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'cannot save test_seed_values: data_type has to be one of [int,boolean,float,date,time,datetime,timestamp,unix_timestamp,string]';
+          END IF;
+          
+          
+        END
+SQL;
+        $this->execute($trigger);
+
+
+        $trigger = <<<SQL
+        CREATE TRIGGER trigger_before_create_test_seed_values
+            BEFORE INSERT ON test_seed_values
+            FOR EACH ROW
+        BEGIN
+          IF NEW.data_type not in ('int','boolean','float','date','time','datetime','timestamp','unix_timestamp','string')
+          THEN
+               SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'cannot save test_seed_values: data_type has to be one of [int,boolean,float,date,time,datetime,timestamp,unix_timestamp,string]';
+          END IF;
+          
+          
+          
+        END
+SQL;
+        $this->execute($trigger);
+    }
+
+
+    public function down()
+    {
+
+        $this->dropTable('test_seed_values');
+        $this->dropTable('test_seeds');
+        $this->dropTable('test_resource_locks');
+        $this->dropTable('test_resources');
+        $this->dropTable('test_runs'); //drops triggers too
+        $this->dropTable('tests');
+    }
+}
+
+/*
+
+
+
+table: test_seed_values
+This is the actual seed data. Numeric and date time data can be cast, if necessary. For example if one field uses a timestamp can enter a human date time here for ease of use and editing
+id (primary)
+test_resource_seed_id (foreign key)
+data_type (var char)
+field_name (var char)
+field_value (var char)
+
+ */
